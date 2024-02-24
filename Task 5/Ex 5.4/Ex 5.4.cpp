d355c032bffab82f2fe855df8d9978288c5f109b
Sergey Afanasyev <cj-mark@yandex.ru>
2024-02-24 20:44:41 +0300

    Add Lab 5

---
+++ Task 5/Ex 5.4/Ex 5.4.cpp
@@ -0,0 +1,70 @@
+﻿#include <iostream>
+#include <string>
+#include <cstring>
+#include <windows.h>
+#include <vector>
+using namespace std;
+
+void funcResult(string, int&, bool&);
+
+int main()
+{
+	string inputStroka;
+	vector<string> argv;
+	int argc = 0, result = 0;
+	bool flag = true;
+	getline(cin, inputStroka);
+	string cnt;
+	for (int i = 0; i < inputStroka.length(); i++)
+	{
+		if (inputStroka[i] == ' ' || i == inputStroka.length() - 1)
+		{
+			argv.push_back(cnt);
+			argc++;
+			cnt = "";
+		}
+		else
+			cnt += inputStroka[i];
+	}
+	funcResult(inputStroka, result, flag);
+	if (flag == true)
+		cout << "Result: " << result;
+	else
+		cout << "ERROR!";
+}
+
+void funcResult(string inputStroka, int &result, bool &flag)
+{
+	vector<string> argv;
+	string cnt;
+	for (int i = 0; i <= inputStroka.length(); ++i)
+	{
+		if (inputStroka[i] == ' ' || i == inputStroka.length())
+		{
+			argv.push_back(cnt);
+			cnt = "";
+		}
+		else
+			cnt += inputStroka[i];
+	}
+	for (int i = 0; i < argv.size(); i++)
+	{
+		cout << argv[i] << endl;
+	}
+	if (argv.size() != 4 || strncmp(argv[0].c_str(), "nameProg", 8) != 0)
+	{
+		cout << strncmp(argv[0].c_str(), "nameProg", 8);
+		flag = false;
+	}
+	else
+	{
+		if (strncmp(argv[1].c_str(), "-a", 2) == 0)
+			result = atoi(argv[2].c_str()) + atoi(argv[3].c_str());
+		else if (strncmp(argv[1].c_str(), "-m", 2) == 0)
+			result = atoi(argv[2].c_str()) * atoi(argv[3].c_str());
+		else
+			flag = false;
+	}
+}
+
+
---
+++ Task 5/Task 5.4.1/Task 5.4.1.cpp
@@ -0,0 +1,92 @@
+﻿#include <iostream>
+#include <windows.h>
+using namespace std;
+
+int main()
+{
+    SetConsoleOutputCP(1251);
+    int n;
+    cout << "Введите число элементов массива: ";
+    cin >> n;
+    int* mas = new int[n];
+    for (int i = 0; i < n; i++)
+    {
+        cout << "mas[" << i << "]=";
+        cin >> mas[i];
+    }
+    int s = 0;
+    bool flagNeg = false, flagPos = false;
+    int sumNeg = 0, sumPos = 0;
+    int sumEven = 0, sumOdd = 0;
+    int maxVal, maxIndex, minVal, minIndex;
+    for (int i = 0; i < n; i++)
+    {
+        s += mas[i];
+        if (mas[i] < 0)
+        {
+            sumNeg += mas[i];
+            flagNeg = true;
+        }
+        else
+        {
+            sumPos += mas[i];
+            flagPos = true;
+        }
+        if (i % 2 == 0)
+            sumEven += mas[i];
+        else
+            sumOdd += mas[i];
+        if (i == 0)
+        {
+            maxVal = mas[i];
+            maxIndex = i;
+            minVal = mas[i];
+            minIndex = i;
+        }
+        else
+        {
+            if (mas[i] > maxVal)
+            {
+                maxVal = mas[i];
+                maxIndex = i;
+            }
+            if (mas[i] < minVal)
+            {
+                minVal = mas[i];
+                minIndex = i;
+            }
+        }
+    }
+    cout << "Сумма значений элементов массива: " << s << endl;
+    double mid; // среднее значение элементов
+    mid = double(s) / n;
+    cout << "Среднее значение элементов массива: " << mid << endl;
+    if (flagNeg == true)
+        cout << "Сумма отрицательных элементов массива: " << sumNeg << endl;
+    else
+        cout << "В массиве нет отрицательных элементов!" << endl;
+    if (flagPos == true)
+        cout << "Сумма положительных элементов массива: " << sumPos << endl;
+    else
+        cout << "В массиве нет положительных элементов!" << endl;
+    cout << "Сумма четных элементов массива: " << sumEven << endl;
+    cout << "Сумма нечетных элементов массива: " << sumEven << endl;
+    cout << "Максимальное значение элемента массива: " << maxVal << " , его индекс: " << maxIndex << endl;
+    cout << "Минимальное значение элемента массива: " << minVal << " , его индекс: " << minIndex << endl;
+    int sumEntMinMax = 1; // произведение элементов между мин и макс
+    for (int i = min(maxIndex, minIndex); i < max(maxIndex, minIndex) - 1; i++)
+    {
+        if (max(maxIndex, minIndex) - min(maxIndex, minIndex) > 1)
+        {
+            sumEntMinMax *= mas[i + 1];
+        }
+        else
+            sumEntMinMax = 1;
+    }
+    delete[] mas;
+    if (sumEntMinMax == 1)
+        cout << "Между минимальным и максимальным значением нет элементов" << endl;
+    else
+        cout << "Произведение элементов между минимальным и максимальным значением = " << sumEntMinMax << endl;
+    return 0;
+}
