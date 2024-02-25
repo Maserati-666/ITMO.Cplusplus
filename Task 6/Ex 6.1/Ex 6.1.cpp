e7eb8dfc235ce9b36b1bcb4bc1af98b0d58cceea
Sergey Afanasyev <cj-mark@yandex.ru>
2024-02-25 15:17:58 +0300

    Add Lab 6 and 7

---
+++ Task 6/Ex 6.1/Ex 6.1.cpp
@@ -0,0 +1,27 @@
+﻿#include <fstream>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    ofstream os("Text.txt"); // текстовый файл для вывода
+    // проверка, открыт ли файл
+    if (!os)
+    {
+        cout << "Cannot open the file to output. \n";
+    }
+    string text;
+    cout << "Save the lines to HDD. Enter ! to stop." << endl;
+    do
+    {
+        getline(cin, text);
+        if (*text.c_str() == '!') // условие выхода из цикла
+            break;
+        else
+            os << text << endl; // записать в файл строку str
+    } while (true); // бесконечный цикл
+    os.close(); // закрыть файл
+    return 0;
+}
---
+++ Task 7/Ex 7.2/Ex 7.2.cpp
@@ -0,0 +1,54 @@
+﻿#include <iostream> 
+#include <windows.h> 
+
+using namespace std;
+
+struct QuadEquation
+{
+    double root1;
+    double root2;
+};
+
+QuadEquation SolvEquation(bool&);
+
+int main()
+{
+    SetConsoleOutputCP(1251);
+    bool flag = true;
+    QuadEquation equat = SolvEquation(flag);
+    if (flag != true)
+    {
+        cout << "Корней нет!";
+        return -1;
+    }
+    else
+    {
+        cout << "Корень №1 уравнения :" << equat.root1 << endl;
+        cout << "Корень №2 уравнения :" << equat.root2 << endl;
+        return 0;
+    }
+    return 0;
+}
+
+QuadEquation SolvEquation(bool& flag)
+{
+    QuadEquation equat;
+    double a, b, c;
+    double d;
+    cout << "Введите коэффициенты квадратного уравнения (через пробел): ";
+    cin >> a >> b >> c;
+    d = b * b - 4 * a * c;
+    if (d > 0)
+    {
+        equat.root1 = (-b - sqrt(d)) / (2 * a);
+        equat.root2 = (-b + sqrt(d)) / (2 * a);
+    }
+    else if (d == 0)
+    {
+        equat.root1 = -b / (2 * a);
+        equat.root2 = equat.root1;
+    }
+    else
+        flag = false;
+    return equat;
+}
