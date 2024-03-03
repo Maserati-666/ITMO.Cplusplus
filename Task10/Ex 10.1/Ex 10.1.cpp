a4dc1f7d97ee0d3a7c5b6ffa5627cfc822772ae5
Sergey Afanasyev <cj-mark@yandex.ru>
2024-03-03 11:36:01 +0300

    Fix Lab 10

--- Task10/Ex 10.1/Ex 10.1.cpp
+++ Task10/Ex 10.1/Ex 10.1.cpp
@@ -12,9 +12,6 @@ public:
             Triangle::set_dot_one(dot_one);
             Triangle::set_dot_two(dot_two);
             Triangle::set_dot_three(dot_three);
-            side_A = dot_one->distanceTo(*dot_two);
-            side_B = dot_two->distanceTo(*dot_three);
-            side_C = dot_three->distanceTo(*dot_one);
     }
     // Установка вершины 1
     void set_dot_one(Dot* dotOne)
@@ -52,9 +49,25 @@ public:
         return *dot_three;
     }
 
+    // Создание массива длин сторон треугольника
+    double* create_masSide()
+    {
+        double mas[3];
+        mas[0] = dot_one->distanceTo(*dot_two);
+        mas[1] = dot_two->distanceTo(*dot_three);
+        mas[2] = dot_three->distanceTo(*dot_one);
+        return mas;
+    }
+
+
     // Вывод длин сторон треугольника
     void output_len_side()
     {
+        double *mas;
+        mas = create_masSide();
+        double side_A = mas[0];
+        double side_B = mas[1];
+        double side_C = mas[2];
         cout << "Длина стороны A: " << side_A << endl;
         cout << "Длина стороны B: " << side_B << endl;
         cout << "Длина стороны C: " << side_C << endl;
@@ -63,15 +76,19 @@ public:
     // Расчет периметра треугольника
      double calc_perimetr()
     {
-        return side_A + side_B + side_C;
+        double* mas;
+        mas = create_masSide();
+        return mas[0] + mas[1] + mas[2];
     }
 
     // Расчет площади треугольника
     double calc_square()
     {
+        double* mas;
+        mas = create_masSide();
         double square, pp;
         pp = this->calc_perimetr()/ 2;
-        square = sqrt(pp * (pp - side_A) * (pp - side_B) * (pp - side_C));
+        square = sqrt(pp * (pp - mas[0]) * (pp - mas[1]) * (pp - mas[2]));
         return square;
     }
 
@@ -79,9 +96,6 @@ private:
     Dot* dot_one;
     Dot* dot_two;
     Dot* dot_three;
-    double side_A;
-    double side_B;
-    double side_C;
 };
 
 int main()
